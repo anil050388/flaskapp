79733681557babd4157494b948de41ccbd5ee8e8
anil050388 <51058962+anil050388@users.noreply.github.com>
2023-05-10 15:57:57 -0400

    Update app.py
    Commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-
+#
 app = Flask(__name__)
 
 #Applying Secrets
